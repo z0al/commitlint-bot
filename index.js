ac7cd753d48f8b4eaecf34464133b414abc0c844
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-10-30 15:24:11 +0300

    fix: listen to the correct PR actions

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ module.exports = robot => {
 	// For more information on building apps:
 	// https://probot.github.io/docs/
 	robot.on('pull_request.opened', commitlint)
-	robot.on('pull_request.edited', commitlint)
+	robot.on('pull_request.synchronize', commitlint)
 }
