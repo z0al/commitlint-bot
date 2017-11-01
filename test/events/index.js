3bbced46c8f36d595ccd03f372bc1ce93c7a7f90
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-11-01 15:53:45 +0300

    test: fix test fixtures

---
+++ test/events/index.js
@@ -0,0 +1,4 @@
+const opened = require('./pull_request.opened.json')
+const synchronize = require('./pull_request.synchronize.json')
+
+module.exports = { opened, synchronize }
--- test/payload/index.js
+++
@@ -1,4 +0,0 @@
-const opened = require('./pull_request.opened.json')
-const sync = require('./pull_request.synchronize.json')
-
-module.exports = { opened, sync }
