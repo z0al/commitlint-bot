80289e0f4c4c7dac8d1a301ed02398b5f3c59858
Ahmed T. Ali <ah.tajelsir@gmail.com>
2018-02-07 14:56:23 +0200

    test: migrate to Jest

---
+++ test/lint.test.js
@@ -0,0 +1,49 @@
+// Packages
+const { createRobot } = require('probot')
+
+// Ours
+const app = require('../index')
+const events = require('./events')
+
+const repo = { owner: 'user', repo: 'repo' }
+
+let robot, github
+
+beforeEach(() => {
+	// Here we create a robot instance
+	robot = createRobot()
+	// Here we initialize the app on the robot instance
+	app(robot)
+	// Mock GitHub client
+	github = {
+		issues: { createComment: jest.fn() },
+		repos: { createStatus: jest.fn() },
+		pullRequests: {
+			getCommits: jest.fn().mockReturnValue({
+				data: [{ sha: 'abcd', commit: { message: 'bad message' } }]
+			})
+		},
+		paginate: (fn, cb) => cb(fn)
+	}
+	// Passes the mocked out GitHub API into out robot instance
+	robot.auth = () => Promise.resolve(github)
+})
+
+test('status update to pending', async () => {
+	await robot.receive(events.opened)
+	expect(github.repos.createStatus).toHaveBeenCalledWith(
+		expect.objectContaining({ state: 'pending' })
+	)
+})
+
+test('fetching the list of commits', async () => {
+	await robot.receive(events.opened)
+	expect(github.pullRequests.getCommits).toHaveBeenCalledWith(
+		expect.objectContaining({ ...repo, number: 1 })
+	)
+})
+
+// test('comment with errors/warnings', async () => {
+// 	await robot.receive(events.opened)
+// 	expect(github.issues.createComment).toHaveBeenCalled()
+// })
