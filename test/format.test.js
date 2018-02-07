7f4c1c579d2190e144b0ce6dbc5f50a071294b77
Ahmed T. Ali <ah.tajelsir@gmail.com>
2018-02-07 14:56:23 +0200

    test: migrate to Jest

--- test/format.test.js
+++ test/format.test.js
@@ -4,14 +4,17 @@ const expect = require('expect')
 // Ours
 const format = require('../lib/format')
 
-describe('lib/format', () => {
-	const report1 = { commits: {} }
-	const report2 = {
+const values = [
+	// #1
+	{ commits: {} },
+	// #2
+	{
 		commits: {
 			'1': { errors: [], warnings: [{ message: 'warning message' }] }
 		}
-	}
-	const report3 = {
+	},
+	// #3
+	{
 		commits: {
 			'2': {
 				errors: [{ message: 'error message' }],
@@ -19,25 +22,26 @@ describe('lib/format', () => {
 			}
 		}
 	}
+]
 
-	it('generates summary', () => {
-		// Report 1
-		expect(format(report1).summary).toEqual('found 0 problems, 0 warnings')
-		// Report 2
-		expect(format(report2).summary).toEqual('found 0 problems, 1 warnings')
-		// Report 3
-		expect(format(report3).summary).toEqual('found 1 problems, 1 warnings')
-	})
+test('generates summary', () => {
+	// #1
+	expect(format(values[0]).summary).toEqual('found 0 problems, 0 warnings')
+	// #2
+	expect(format(values[1]).summary).toEqual('found 0 problems, 1 warnings')
+	// #3
+	expect(format(values[2]).summary).toEqual('found 1 problems, 1 warnings')
+})
 
-	it('generates comment body', () => {
-		// Report 1
-		expect(format(report1).message).toEqual('')
-		// Report 2
-		expect(format(report2).message).toMatch(/Commit: 1/)
-		expect(format(report2).message).toMatch(/warning message/)
-		// Report 3
-		expect(format(report3).message).toMatch(/Commit: 2/)
-		expect(format(report3).message).toMatch(/error message/)
-		expect(format(report3).message).toMatch(/warning message/)
-	})
+test('generates comment body', () => {
+	// #1
+	expect(format(values[0]).message).toEqual('')
+
+	// #2
+	expect(format(values[1]).message).toMatch(/Commit: 1/)
+	expect(format(values[1]).message).toMatch(/warning message/)
+	// #3
+	expect(format(values[2]).message).toMatch(/Commit: 2/)
+	expect(format(values[2]).message).toMatch(/error message/)
+	expect(format(values[2]).message).toMatch(/warning message/)
 })
