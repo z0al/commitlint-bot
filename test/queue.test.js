10582f6aeb6592446f1fc2414b81246360e7b92a
Ahmed T. Ali <ah.tajelsir@gmail.com>
2018-02-28 15:40:08 +0200

    refactor(queue): pass `store` as a second param instead of `ctx.store`

--- test/queue.test.js
+++ test/queue.test.js
@@ -1,27 +1,28 @@
 // Packages
 const { queue } = require('../lib/queue')
 
-let ctx
+let ctx, store
 
 beforeEach(() => {
+	// Redis
+	store = {
+		save: jest.fn()
+	}
+
 	ctx = {
 		sha: 'abcdefg',
 		repo: () => ({}),
-		// Redis
-		store: {
-			save: jest.fn()
-		},
 		github: { repos: { createStatus: jest.fn() } }
 	}
 })
 
 test('stores context', async () => {
-	await queue(ctx)
-	expect(ctx.store.save).toBeCalledWith(ctx)
+	await queue(ctx, store)
+	expect(store.save).toBeCalledWith(ctx)
 })
 
 test('reports pending status to GitHub', async () => {
-	await queue(ctx)
+	await queue(ctx, store)
 	expect(ctx.github.repos.createStatus).toBeCalledWith(
 		expect.objectContaining({
 			context: 'commitlint',
