8ab3b4fda8d48befb629b8be839178421350472e
Ahmed T. Ali <ah.tajelsir@gmail.com>
2018-02-28 14:08:00 +0200

    fix(store): correctly read installation ID

--- test/store.test.js
+++ test/store.test.js
@@ -9,7 +9,7 @@ jest.mock('redis', () => ({
 				number: 1,
 				owner: 'user',
 				repo: 'test',
-				installation_id: 111
+				id: 111
 			})
 		),
 		set: jest.fn()
@@ -22,16 +22,13 @@ const { createStore } = require('../lib/store')
 
 // Globals
 const DB_URL = (process.env.DB_URL = 'redis://localhost')
-const installation_id = 111
+
 const ctx = {
-	installation_id,
-	issue: obj => ({
-		number: 1,
-		owner: 'user',
-		repo: 'test',
-		installation_id
-	}),
-	payload: { pull_request: { head: { sha: 'commitSHA' } } }
+	issue: obj => ({ number: 1, owner: 'user', repo: 'test', ...obj }),
+	payload: {
+		pull_request: { number: 1, head: { sha: 'commitSHA' } },
+		installation: { id: 111 }
+	}
 }
 
 test('uses DB_URL as redis URL', async () => {
@@ -41,7 +38,7 @@ test('uses DB_URL as redis URL', async () => {
 
 test('stores Context with expiration period', async () => {
 	const client = await createStore()
-	const expCtx = JSON.stringify(ctx.issue({ installation_id }))
+	const expCtx = JSON.stringify(ctx.issue({ id: 111 }))
 
 	await client.save(ctx)
 	expect(redis.createClient().set).toBeCalledWith(
@@ -56,11 +53,9 @@ test('restores Context as Object using commit SHA', async () => {
 	const client = await createStore()
 	await client.save(ctx)
 
-	const head = ctx.payload.pull_request.head.sha
-	const rec = await client.restore(head)
+	const { sha } = ctx.payload.pull_request.head
+	const rec = await client.restore(sha)
 
-	expect(head).toEqual(rec.head)
-	expect(installation_id).toEqual(rec.installation_id)
-	expect(ctx.issue()).toEqual(expect.objectContaining(rec.issue()))
-	expect(ctx.issue()).toEqual(expect.objectContaining(rec.repo()))
+	expect(sha).toEqual(rec.pull_request.head.sha)
+	expect(ctx.payload.installation.id).toEqual(rec.installation.id)
 })
