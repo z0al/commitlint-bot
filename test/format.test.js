9831587964bcba34e0768503d65138d95abe055e
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-10-03 17:40:02 -0300

    fix: improve message
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- test/format.test.js
+++ test/format.test.js
@@ -2,35 +2,43 @@
 const format = require('../lib/format')
 
 const commits = [
-	// #1
-	[
-		{
-			sha: 'abc',
-			errors: [],
-			warnings: [{ message: 'warning message' }]
-		}
-	],
+  // #1
+  [{
+    sha: 'abc',
+    errors: [],
+    warnings: [{
+      message: 'warning message'
+    }]
+  }],
 
-	// #2
-	[
-		{
-			sha: 'def',
-			errors: [{ message: 'error message' }],
-			warnings: [{ message: 'warning message' }]
-		}
-	]
+  // #2
+  [{
+    sha: 'def',
+    errors: [{
+      message: 'error message'
+    }],
+    warnings: [{
+      message: 'warning message'
+    }]
+  }]
 ]
 
-test('repalces placeholder', () => {
-	expect(format(commits[0])).not.toMatch(/PLACEHOLDER/)
+test('replaces placeholder', () => {
+  expect(format(commits[0])).not.toMatch(/PLACEHOLDER/)
+})
+
+test('replaces details', () => {
+  expect(format(commits[0])).not.toMatch(/DETAILS/)
 })
 
 test('generates comment body', () => {
-	// #1
-	expect(format(commits[0])).toMatch(/Commit: abc/)
-	expect(format(commits[0])).toMatch(/warning message/)
-	// #2
-	expect(format(commits[1])).toMatch(/Commit: def/)
-	expect(format(commits[1])).toMatch(/error message/)
-	expect(format(commits[1])).toMatch(/warning message/)
+  // #1
+  expect(format(commits[0])).toMatch(/Commit: abc/)
+  expect(format(commits[0])).toMatch(/warning message/)
+  // #2
+  expect(format(commits[1])).toMatch(/Commit: def/)
+  expect(format(commits[1])).toMatch(/error message/)
+  expect(format(commits[1])).toMatch(/warning message/)
+  // test both
+  expect(format(commits.flat())).toMatch(/\* abc\n\* def/)
 })
