dfba0b74c478d47768cc671954b417f8b062973b
Ahmed T. Ali <ah.tajelsir@gmail.com>
2019-10-11 14:58:28 +0300

    build(deps): upgrade commitlint

--- test/events/index.js
+++ test/events/index.js
@@ -1,4 +1,4 @@
-const opened = require('./pull_request.opened.json')
-const synchronize = require('./pull_request.synchronize.json')
+const opened = require('./pull_request.opened.json');
+const synchronize = require('./pull_request.synchronize.json');
 
-module.exports = { opened, synchronize }
+module.exports = { opened, synchronize };
--- test/format.test.js
+++ test/format.test.js
@@ -1,44 +1,54 @@
 // Ours
-const format = require('../lib/format')
+const format = require('../lib/format');
 
 const commits = [
-  // #1
-  [{
-    sha: 'abc',
-    errors: [],
-    warnings: [{
-      message: 'warning message'
-    }]
-  }],
+	// #1
+	[
+		{
+			sha: 'abc',
+			errors: [],
+			warnings: [
+				{
+					message: 'warning message'
+				}
+			]
+		}
+	],
 
-  // #2
-  [{
-    sha: 'def',
-    errors: [{
-      message: 'error message'
-    }],
-    warnings: [{
-      message: 'warning message'
-    }]
-  }]
-]
+	// #2
+	[
+		{
+			sha: 'def',
+			errors: [
+				{
+					message: 'error message'
+				}
+			],
+			warnings: [
+				{
+					message: 'warning message'
+				}
+			]
+		}
+	]
+];
 
 test('replaces placeholder', () => {
-  expect(format(commits[0])).not.toMatch(/PLACEHOLDER/)
-})
+	expect(format(commits[0])).not.toMatch(/PLACEHOLDER/);
+});
 
 test('replaces details', () => {
-  expect(format(commits[0])).not.toMatch(/DETAILS/)
-})
+	expect(format(commits[0])).not.toMatch(/DETAILS/);
+});
 
 test('generates comment body', () => {
-  // #1
-  expect(format(commits[0])).toMatch(/Commit: abc/)
-  expect(format(commits[0])).toMatch(/warning message/)
-  // #2
-  expect(format(commits[1])).toMatch(/Commit: def/)
-  expect(format(commits[1])).toMatch(/error message/)
-  expect(format(commits[1])).toMatch(/warning message/)
-  // test both
-  expect(format(commits.flat())).toMatch(/\* abc\n\* def/)
-})
+	// #1
+	expect(format(commits[0])).toMatch(/Commit: abc/);
+	expect(format(commits[0])).toMatch(/warning message/);
+	// #2
+	expect(format(commits[1])).toMatch(/Commit: def/);
+	expect(format(commits[1])).toMatch(/error message/);
+	expect(format(commits[1])).toMatch(/warning message/);
+	// test both
+	expect(format(commits.flat())).toMatch(/\* abc\n\* def/);
+});
